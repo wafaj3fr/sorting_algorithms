01022c301d64f7dee00bd2c63a8f3e7fe52da0dc
wafa <wafaagaafar@gmail.com>
2024-01-23 12:06:55 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,46 @@
 #include "sort.h"
-
 /**
- * swap - swaps two nodes
- * @head: pointer to head
- * @node1: pointer to first node
- * @node2: pointer to second node
+ * insertion_sort_list - sorts a doubly linked list based on Insertion sort algorithm
+ * @list: the list to be sorted
  */
-
-void swap(listint_t **head, listint_t *node1, listint_t *node2)
+void insertion_sort_list(listint_t **list)
 {
-	(*node1)->next = node2->next;
-	if (node2->next != NULL)
-		node2->next->prev = *node1;
-	node2->prev = (*node1)->prev;
-	node2->next = *node1;
-	if ((*node1)->prev != NULL)
-		(*node1)->prev->next = node2;
-	else
-		*head = node2;
-	(*node1)->prev = n2;
-	*n1 = n2->prev;
+	listint_t *n;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	n = (*list)->next;
+	while (n)
+	{
+		while ((n->prev) && (n->prev->n > n->n))
+		{
+			n = swap_node(n, list);
+			print_list(*list);
+		}
+		n = n->next;
+	}
 }
 
 /**
- * insertion_sort_list - sorts a list based on insertion sort algorithm
- * @list: pointer to head
+ * swap_nodes - swaps two nodes
+ * @n: node
+ * @l: list
+ * Return: pointer to a node
  */
 
-void insertion_sort_list(listint_t **list)
+listint_t *swap_nodes(listint_t *n, listint_t **l)
 {
-	listint_t i, x, t;
+	listint_t *backward = n->prev, *recent = n;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-	for (i = (*list)->next; i != NULL; i = t)
-	{
-		t = i->next;
-		x = i->prev;
-		while (x != NULL && i->n < x->n)
-		{
-			swap(list, &x, i);
-			print_list((const listint_t *)*list);
-		}
-	}
+	backward->next = recent->next;
+	if (recent->next)
+		recent->next->prev = backward;
+	recent->next = backward;
+	recent->prev = backward->prev;
+	backward->prev = recent;
+	if (recent->prev)
+		recent->prev->next = recent;
+	else
+		*l = recent;
+	return (recent);
 }
